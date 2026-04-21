c661fed9ec94a9d54506ee24c2ae30c70d50b857
若安 <3172683060@qq.com>
2026-04-21 21:54:23 +0800

    create Brick and Java

---
+++ Brick/Main.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+/**
+ * 主类 Main，用于启动砖块破坏者游戏窗口
+ * 该类包含程序的入口点 main 方法
+ */
+public class Main {
+    public static void main(String[] args) {
+         // 创建一个JFrame窗口对象，标题为"Brick Breaker Game"
+         JFrame frame = new JFrame("Brick Breaker Game");
+         // 设置窗口可见
+         frame.setVisible(true);
+    }
+}
